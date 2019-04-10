d46d663b7e985939af8b89545870c18a83c80c60
james_stocktonj <james.stocktonj@gmail.com>
2019-04-10 08:10:25 +0100

    Update chordfill.py

--- src/chordfill.py
+++ src/chordfill.py
@@ -5,7 +5,8 @@
 from chord import *
 from keyfinder import *
 
-mel = [0, 2, 4, 6, 3, 5, 9]
+mel = [0, 2, 4, 6, 3, 7, 12, 9]
+testChords = [0, 0, 0, 0, 0, 0, 0, 0]
 
 chordPreference = [1, 4, 5, 6, 3, 2, 7]
 
@@ -66,7 +67,68 @@ def endChord(melody, scale):
          - scale - the array of the decided key scale
 """
 def getCadence(melody, chords, scale):
-    pass
+    cadenceError = False
+
+    #finds the final chords and sets the chord array to that value
+    finChord = endChord(melody, scale)
+    chords[len(melody)-1] = finChord
+
+    #gets the penultimate note from the melody array
+    penultimateNote = noteUniversal(melody[len(melody) - 2])
+
+    #handles perfect cadences
+    if finChord == 1:
+        if penultimateNote in getChord(scale, 5, False):
+            chords[len(melody)-2] = 5
+        elif penultimateNote in getChord(scale, 4, False):
+            chords[len(melody)-2] = 4
+        elif penultimateNote in getChord(scale, 1, False):
+            if noteUniversal(melody[len(melody) - 3]) in getChord(scale, 5, False):
+                chords[len(melody)-2] = 1
+                chords[len(melody)-3] = 5
+            elif noteUniversal(melody[len(melody) - 3]) in getChord(scale, 4, False):
+                chords[len(melody)-2] = 1
+                chords[len(melody)-3] = 4
+            else:
+                cadenceError = True
+        else:
+            cadenceError = True
+
+    #handles imperfect cadence
+    elif finChord == 5:
+        if penultimateNote in getChord(scale, 1, False):
+            chords[len(melody)-2] = 1
+        elif penultimateNot in getChord(scale, 4, False):
+            chords[len(melody)-2] = 4
+        else:
+            cadenceError = True
+
+    elif finChord == 4:
+        if penultimateNote in getChord(scale, 1, False):
+            chords[len(melody)-2] = 1
+        elif penultimateNote in getChord(scale, 5, False):
+            chords[len(melody)-2] = 5
+        else:
+            cadenceError = True
+
+    elif finChord == 6:
+        if penultimateNote in getChord(scale, 1, False):
+            chords[len(melody)-2] = 1
+        else:
+            cadenceError = True
+
+    else:
+        #cadence error if no last chord is found
+        smalestDif = [0, -1, 0]         #note, difference, chord num
+
+        
+        for i in range(0, 10):
+
+            pass
+
+    #if there is a cadence error
+    if cadenceError:
+        pass
     
 
     
